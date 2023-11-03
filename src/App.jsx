19df97221cb624b67ff3c15ccc93add874a29b35
Daranut <633120100102@rmu.ac.th>
2023-11-03 15:58:18 +0700

    Lanning

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      
+        <h1 style={{ color:'pink',fontSize: 50}}>DARANUT</h1>
+        <h2 className='text-purple'>wed Programming</h2>
+
+        <button className="btn">click</button><br />
+        <img className='picture' src="https://picsum.photos/300/200?random=1" alt="pic" />
+        <img className='picture'  src="https://picsum.photos/300/200?random=2" alt="pic" />
+        <img className='picture'  src="https://picsum.photos/300/200?random=3" alt="pic" />
+        <img className='picture' src="https://picsum.photos/300/200?random=4" alt="pic" />
+        <img className='picture' src="https://picsum.photos/300/200?random=5" alt="pic" />
+        <img className='picture' src="https://picsum.photos/300/200?random=6" alt="pic" />
+        <img className='picture' src="https://picsum.photos/300/200?random=7" alt="pic" />
+        <img className='picture' src="https://picsum.photos/300/200?random=8" alt="pic" />
+        <img className='picture' src="https://picsum.photos/300/200?random=9" alt="pic" />
+        <img className='picture' src="https://picsum.photos/300/200?random=10" alt="pic" />
+        <img className='picture' src="https://picsum.photos/300/200?random=11" alt="pic" />
+        <img className='picture' src="https://picsum.photos/300/200?random=12" alt="pic" />
+    </div>
+  )
+}
